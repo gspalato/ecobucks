4ba1ff353de7dc9b89b3dc650bc813092b48200d
Gabriel <3950465+gspalato@users.noreply.github.com>
2023-10-03 10:07:11 -0300

    Added color prop to BackButton.

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,15 +1,21 @@
 import { Feather } from '@expo/vector-icons';
 import { router } from 'expo-router';
-import { StyleProp, TouchableOpacity, View } from 'react-native';
+import {
+	OpaqueColorValue,
+	StyleProp,
+	TouchableOpacity,
+	View,
+} from 'react-native';
 import { ViewStyle } from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 import tw from 'twrnc';
 
 type BackButtonProps = {
+	color?: string | OpaqueColorValue;
 	style?: StyleProp<ViewStyle>;
 };
 
 const Component: React.FC<BackButtonProps> = (props) => {
-	const { style } = props;
+	const { color, style } = props;
 
 	const onPress = () => {
 		router.back();
@@ -18,7 +24,7 @@ const Component: React.FC<BackButtonProps> = (props) => {
 	return (
 		<TouchableOpacity hitSlop={50} onPress={onPress}>
 			<View style={style}>
-				<Feather size={35} name='chevron-left' color={'#000000'} />
+				<Feather size={35} name='chevron-left' color={color} />
 			</View>
 		</TouchableOpacity>
 	);
