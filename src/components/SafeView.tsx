171d90c1da44896a58229d60c4a4635a0ae9d27d
Gabriel <3950465+gspalato@users.noreply.github.com>
2023-10-06 05:02:17 -0300

    SafeView now supports all SafeAreaView props.

--- src/components/SafeView.tsx
+++ src/components/SafeView.tsx
@@ -1,20 +1,27 @@
 import { StyleProp, ViewStyle } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+	NativeSafeAreaViewProps,
+	SafeAreaView,
+} from 'react-native-safe-area-context';
 import tw from 'twrnc';
 
 import { usePlatform } from '@/lib/platform';
 
 type SafeViewProps = {
 	style?: StyleProp<ViewStyle>;
-} & React.PropsWithChildren;
+} & React.PropsWithChildren &
+	NativeSafeAreaViewProps;
 
 const Component: React.FC<SafeViewProps> = (props) => {
-	const { children, style } = props;
+	const { children, style, ...rest } = props;
 
 	const { isAndroid, SafeAreaStyle } = usePlatform();
 
 	return (
-		<SafeAreaView style={[SafeAreaStyle, style, isAndroid && tw`pt-0`]}>
+		<SafeAreaView
+			{...rest}
+			style={[SafeAreaStyle, style, isAndroid && tw`pt-0`]}
+		>
 			{children}
 		</SafeAreaView>
 	);
