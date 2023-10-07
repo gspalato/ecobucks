c45028db630d1e36c44ee8d6c73009f5f285210f
Gabriel <3950465+gspalato@users.noreply.github.com>
2023-10-07 18:03:05 -0300

    Fixed @lib/layout exporting.

---
+++ src/lib/layout/index.tsx
@@ -0,0 +1,5 @@
+import { HeaderContextValue, HeaderProvider, useHeaderLayout } from './header';
+import { TabBarContextValue, TabBarProvider, useTabBarLayout } from './tabbar';
+
+export { HeaderContextValue, HeaderProvider, useHeaderLayout };
+export { TabBarContextValue, TabBarProvider, useTabBarLayout };
